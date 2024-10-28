793c137c94858123f91048c1880f7d5d20558b5f
Akshay Kumar Mandlem <136102919+akshaykumarmandlem@users.noreply.github.com>
2024-10-28 12:09:29 -0600

    Delete Terraform EKS directory

--- Terraform EKS/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from .kubernetes_detection import analyze_kubernetes_threats
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    threats = analyze_kubernetes_threats()
-    return render_template('index.html', threats=threats)
--- Terraform EKS/output.tf
+++
@@ -1,8 +0,0 @@
-# Outputs for the EKS cluster and EC2 instance
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_id
-}
